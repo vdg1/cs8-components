92d1c5b6f52349f708beeedbd482139cc42a04df
vdg1 <volker.drewer@gmail.com>
2020-06-07 15:08:39 +0200

    Added test application

---
+++ cs8Components/cs8ProgramComponent/testCS8App_ReadWrite/main.cpp
@@ -0,0 +1,16 @@
+#include "cs8ProjectComponent.h"
+#include "cs8application.h"
+#include <QCoreApplication>
+#include <QDomDocument>
+#include <QTableView>
+
+int main(int argc, char *argv[]) {
+  QCoreApplication a(argc, argv);
+
+  cs8Application app;
+  app.open("D:\\data\\Staubli\\SRS\\_Development\\Dev_SAXEAutomation_"
+           "CS9\\Controller_s882\\usr\\usrapp\\aMotionID\\aMotionID.pjx");
+  app.save("D:\\data\\Staubli\\SRS\\_Development\\Dev_SAXEAutomation_"
+           "CS9\\Controller_s882\\usr\\usrapp\\aMotionID_saved\\",
+           "aMotionID_saved", false);
+}
